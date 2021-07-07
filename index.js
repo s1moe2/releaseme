8a0701a2b267ea6e04e8b537fcc5b15586a25ab5
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-07-07 17:25:36 +0100

    feat: animals

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use('/', (req, res) => res.status(200).send(version))
 
 app.use('/users', (req, res) => res.status(200).send(users))
 
+app.use('/animals', (req, res) => res.status(200).send(users))
+
 app.use((err, req, res, next) => res.status(500).json({ message: 'internal error' }))
 
 app.listen(5000, () => console.log('listening'))
