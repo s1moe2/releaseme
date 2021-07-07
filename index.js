e2c0a5a2b053e810fbdf09b6915761e639aacf5b
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-07-07 16:13:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const users = []
+
+const version = fs.readFileSync('./version')
+app.use('/', (req, res) => res.status(200).send(version))
+
+app.use('/users', (req, res) => res.status(200).send(users))
+
+app.use((err, req, res, next) => res.status(500).json({ message: 'internal error' }))
+
+app.listen(5000, () => console.log('listening'))
