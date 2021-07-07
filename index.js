50faa052918d63d1c6e48a84c3de06f4c7f59c9e
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-07-07 19:43:39 +0100

    feat: removed route BREAKING CHANGE: removed deprecated

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use('/zoo', (req, res) => res.status(200).send(version))
 app.use('/users', (req, res) => res.status(200).send(users))
 
 app.use('/animals', (req, res) => res.status(200).send({}))
-app.use('/facilities', (req, res) => res.statusStatus(204))
 
 app.use((err, req, res, next) => res.status(500).json({ message: 'internal error' }))
 
